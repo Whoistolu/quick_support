3a89d043601d6f78e4a8593a3cc28c160b583159
Whoistolu <digittolu25@gmail.com>
2025-06-03 00:27:55 +0100

    Update the TicketType association

--- app/graphql/types/ticket_type.rb
+++ app/graphql/types/ticket_type.rb
@@ -9,5 +9,8 @@ module Types
     field :user_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :user, Types::UserType, null: false
+    field :comments, [ Types::CommentType ], null: true
   end
 end
