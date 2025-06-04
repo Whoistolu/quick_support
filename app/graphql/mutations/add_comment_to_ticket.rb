4912280410cd5e59fe070aa94c843e97bb9b2d35
Whoistolu <digittolu25@gmail.com>
2025-06-04 15:09:43 +0100

    Add arguments and resolve method to AddCommentToTicket mutation

--- app/graphql/mutations/add_comment_to_ticket.rb
+++ app/graphql/mutations/add_comment_to_ticket.rb
@@ -2,15 +2,26 @@
 
 module Mutations
   class AddCommentToTicket < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    argument :ticket_id, ID, required: true
+    argument :user_id, ID, required: true
+    argument :body, String, required: true
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    field :comment, Types::CommentType, null: true
+    field :errors, [ String ], null: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(ticket_id:, user_id:, body:)
+      ticket = Ticket.find_by(id: ticket_id)
+      user = User.find_by(id: user_id)
+
+      return { comment: nil, errors: [ "Ticket or User not found" ] } if ticket.nil? || user.nil?
+
+      comment = Comment.new(ticket: ticket, user: user, body: body)
+
+      if comment.save
+        { comment: comment, errors: [] }
+      else
+        { comment: nil, errors: comment.errors.full_messages }
+      end
+    end
   end
 end
