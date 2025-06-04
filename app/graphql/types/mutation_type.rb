1dbf650938588e145f54dc86c57a41502e401927
Whoistolu <digittolu25@gmail.com>
2025-06-04 14:55:17 +0100

    Add field :create_ticket to MutationType

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :create_ticket, mutation: Mutations::CreateTicket
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
