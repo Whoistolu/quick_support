3390e60fab6363b035ab44d18f9db830669f04f5
Whoistolu <digittolu25@gmail.com>
2025-06-04 15:11:21 +0100

    Remove unused fields and TODO comment from MutationType

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,11 +4,5 @@ module Types
   class MutationType < Types::BaseObject
     field :add_comment_to_ticket, mutation: Mutations::AddCommentToTicket
     field :create_ticket, mutation: Mutations::CreateTicket
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
