c63d457f9a071d1de6ec92dd78248ccf5498b9f6
Whoistolu <digittolu25@gmail.com>
2025-06-04 15:08:33 +0100

    Add subject, description, and status arguments to CreateTicket mutation

--- app/graphql/mutations/create_ticket.rb
+++ app/graphql/mutations/create_ticket.rb
@@ -2,6 +2,26 @@
 
 module Mutations
   class CreateTicket < BaseMutation
+    argument :subject, String, required: true
+    argument :description, String, required: true
+    argument :user_id, ID, required: true
+    argument :status, String, required: false, default_value: "open"
 
+    field :ticket, Types::TicketType, null: true
+    field :errors, [ String ], null: false
+
+
+    def resolve(subject:, description:, user_id:)
+      user = User.find_by(id: user_id)
+      return { ticket: nil, errors: [ "User not found" ] } unless user
+
+      ticket = user.tickets.build(subject: subject, description: description, status: "open")
+
+      if ticket.save
+        { ticket: ticket, errors: [] }
+      else
+        { ticket: nil, errors: ticket.errors.full_messages }
+      end
+    end
   end
 end
